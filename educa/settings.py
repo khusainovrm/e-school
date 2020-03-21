2856a3324a3c18448663559abcccc4f9bae02dea
Rinat <37953890+khusainovrm@users.noreply.github.com>
2020-03-21 08:27:05 +0100

    new css and minor refactoring

--- educa/settings.py
+++ educa/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'courses/static'),
+]
